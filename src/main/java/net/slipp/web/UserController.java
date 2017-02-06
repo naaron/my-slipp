b2284a92ce75e80243893ae57b2642a6288024ba
hide305-PC\hide305 <hide305@naver.com>
2017-02-06 13:25:40 +0900

    html 템플릿, url 리팩토링

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,27 +1,54 @@
 package net.slipp.web;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>(); 
 	
-	@PostMapping("/create")
-	public String create(User user) {
-		users.add(user);
-		return "redirect:/list";
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
 	}
 	
-	@GetMapping("/list")
+	@PostMapping("")
+	public String create(User user) {
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
+	}
+	
+	@GetMapping("{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findOne(id);
+		model.addAttribute("user", user);
+		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findOne(id);
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
