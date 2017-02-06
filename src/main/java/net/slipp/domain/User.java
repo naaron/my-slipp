b1b423dfa4e823fd452583b9cdf3abd29e19bd57
hide305-PC\hide305 <hide305@naver.com>
2017-02-06 13:45:48 +0900

    로그인 기능완료

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -26,6 +26,9 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public String getPassword() {
+		return password;
+	}
 
 	public void setName(String name) {
 		this.name = name;
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,6 +1,8 @@
 package net.slipp.web;
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +21,31 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		
+		if(user == null) {
+			System.out.println("Login Failed!");
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!password.equals(user.getPassword())) {
+			System.out.println("Login Failed!");
+			return "redirect:/users/loginForm";
+		}
+		
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
 	
 	@GetMapping("/form")
 	public String form() {
