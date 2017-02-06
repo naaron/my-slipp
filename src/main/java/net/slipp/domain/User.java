1a31a9346a1c998a252596bead8ce0656fde51ec
hide305-PC\hide305 <hide305@naver.com>
2017-02-06 16:34:05 +0900

    질문등록 / 리스트

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -33,6 +33,10 @@ public class User {
 		this.userId = userId;
 	}
 
+	public String getUserId() {
+		return userId;
+	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -1,15 +1,42 @@
 package net.slipp.web;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.slipp.domain.Question;
+import net.slipp.domain.QuestionRepository;
+import net.slipp.domain.User;
+
 @Controller
-@RequestMapping("/qna")
+@RequestMapping("/questions")
 public class QuestionController {
 	
-	@GetMapping("/qnaForm")
-	public String qna() {
+	@Autowired
+	private QuestionRepository questionRepository; 
+	
+	@GetMapping("/form")
+	public String form(HttpSession session) {
+		if(!HttpSessionUtils.isLoginUser(session)){
+			return "/users/loginForm";
+		}
 		return "/qna/form";
+	}
+	
+	@PostMapping("")
+	public String create(String title, String contents, HttpSession session) {
+		if(!HttpSessionUtils.isLoginUser(session)){
+			return "/users/loginForm";
+		}
+		
+		User sessionUser = HttpSessionUtils.getUserFromSession(session);
+		Question newQuestion = new Question(sessionUser.getUserId(), title, contents);
+		questionRepository.save(newQuestion);
+		
+		return "redirect:/";
 	}
 }
