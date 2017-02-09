da5892ce0651455df394256dd8f40e476a44c507
hide305-PC\hide305 <hide305@naver.com>
2017-02-09 10:35:47 +0900

    질문 상세

--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,5 +40,12 @@ public class QuestionController {
 		questionRepository.save(newQuestion);
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("{id}")
+	public String show(@PathVariable Long id, Model model) {
+		Question viewQuestion = questionRepository.findOne(id);
+		model.addAttribute("show", viewQuestion);
+		return "/qna/show";
 	}
 }
