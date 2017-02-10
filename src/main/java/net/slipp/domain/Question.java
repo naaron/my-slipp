8ffc6497df451f14c7d9e2670fba89c5b4da2946
hide305-PC\hide305 <hide305@naver.com>
2017-02-10 14:06:49 +0900

    localdatetime 리팩토링

--- src/main/java/net/slipp/domain/Question.java
+++ src/main/java/net/slipp/domain/Question.java
@@ -20,12 +20,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 @Entity
-public class Question {
-	@Id
-	@GeneratedValue
-	@JsonProperty
-	private Long id;
-	
+public class Question extends AbstractEntity {
 	@ManyToOne
 	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
 	@JsonProperty
@@ -41,8 +36,6 @@ public class Question {
 	@JsonProperty
 	private Integer countOfAnswer = 0;
 	
-	private LocalDateTime createDate;
-	
 	@OneToMany(mappedBy = "question")
 	@OrderBy("id ASC")
 	private List<Answer> answer;
@@ -53,7 +46,6 @@ public class Question {
 		this.writer = writer;
 		this.title = title;
 		this.contents = contents;
-		this.createDate = LocalDateTime.now();
 	}
 	
 	/*
@@ -74,18 +66,6 @@ public class Question {
 		this.countOfAnswer -= 1;
 	}
 
-	/*
-	 * 2017.02.10
-	 * 날짜 포멧 ex)2017.02.10 10:10:10
-	 * author : aron
-	 */
-	public String getFormattedCreateDate() {
-		if(createDate == null){
-			return "";
-		}
-		return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
-	}
-	
 	public void update(String title, String contents) {
 		this.title = title;
 		this.contents = contents;
@@ -96,30 +76,6 @@ public class Question {
 		return this.writer.equals(loginUser);
 	}
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Question other = (Question) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 
 	
 	
--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -9,12 +9,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
-public class User {
-	
-	@Id
-	@GeneratedValue
-	@JsonProperty
-	private Long id;
+public class User extends AbstractEntity {
 	
 	@Column(nullable=false, length=20, unique=true)
 	@JsonProperty
@@ -28,17 +23,6 @@ public class User {
 	
 	@JsonProperty
 	private String email;
-
-	public Long getId() {
-		return id;
-	}
-	
-	public boolean matchId(Long newId) {
-		if(newId == null){
-			return false;
-		}
-		return newId.equals(id);
-	}
 
 	public void setUserId(String userId) {
 		this.userId = userId;
@@ -67,37 +51,16 @@ public class User {
 	}
 
 	public void update(User newUser) {
-		// TODO Auto-generated method stub
 		this.password = newUser.password;
 		this.name = newUser.name;
 		this.email = newUser.email;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	public boolean matchId(Long newId) {
+		if(newId == null){
+			return false;
+		}
+		return newId.equals(getId());
 	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
 
 }
